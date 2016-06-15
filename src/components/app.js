637f8ede87eeb3459a9304e07a557166df378b61
Muze_Bank <p1vongviseskij@gmail.com>
2016-06-15 23:38:28 +0700

    add greeting message

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            value: ''
+        }
+    }
+
+    onInputChange(e){
+        let value = e.target.value;
+        this.setState({value});
+    }
+
+    render() {
+        return (
+            <div>
+                <input onChange={this.onInputChange.bind(this)} value={this.state.value} placeholder="type something here"/>
+                <div>
+                    Hey!!! Welcome to React world <p/>
+                    {this.state.value}
+                </div>
+            </div>
+        );
+    }
 }
+
+export default App;
